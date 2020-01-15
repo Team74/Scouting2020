106ee0f081e18892720f8f8b24077604248aee71
IfElifElse <carlthechimchar@gmail.com>
2020-01-14 20:32:57 -0500

    Changed Teleop code

--- app/src/main/java/com/example/chaos/scouting2020/TeleopActivity.java
+++ app/src/main/java/com/example/chaos/scouting2020/TeleopActivity.java
@@ -5,16 +5,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TeleopActivity extends AppCompatActivity {
+    private int highGoalNumber = 0;
+    protected void displayHighGoalNumber() {
+        TextView teleopHighGoalNumberText = (TextView) findViewById(R.id.teleopHighGoalNumberText);
+        teleopHighGoalNumberText.setText("" + highGoalNumber);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teleop);
+        displayHighGoalNumber();
     }
     public void endgameButtonPressed(View endgameButton) {
         Intent intent = new Intent(this, EndgameActivity.class);
         startActivity(intent);
+    }
+    public void teleopHighGoalPlusButtonPressed(View teleopHighGoalPlusButton) {
+        highGoalNumber = highGoalNumber + 1;
+        displayHighGoalNumber();
+    }
+    public void teleopHighGoalMinusButtonPressed(View teleopHighGoalMinusButton) {
+        highGoalNumber = highGoalNumber - 1;
+        displayHighGoalNumber();
     }
 }
