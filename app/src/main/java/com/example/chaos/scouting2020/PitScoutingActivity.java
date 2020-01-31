007783785a3e1cff24ace4d74f52d84d0ea5621d
chaos <haha@yeet.lol>
2020-01-30 20:39:09 -0500

    Opinion Database thing plus Endgame auton button fix

---
+++ app/src/main/java/com/example/chaos/scouting2020/PitScoutingActivity.java
@@ -0,0 +1,15 @@
+package com.example.chaos.scouting2020;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PitScoutingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pit_scouting);
+    }
+}
+
